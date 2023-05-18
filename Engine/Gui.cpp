cfd6a1522297f6ff0a32ae82caa1958655911316
Wildric <wildric.auric@gmail.com>
2023-05-19 01:38:03 +0200

    implemented imgui for ui

---
+++ Engine/Gui.cpp
@@ -0,0 +1,31 @@
+#include "glew.h"
+#include "glfw3.h"
+#include "Context.h"
+
+#include "Gui.h"
+
+void Gui::Init() {
+	ImGui::CreateContext();
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL((GLFWwindow*)Context::window, true);
+	ImGui_ImplOpenGL3_Init();
+	static ImGuiIO& io = ImGui::GetIO();
+	ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	io.Fonts->AddFontFromFileTTF("Resources\\fonts\\arial.ttf", 16.0f);
+}
+
+void Gui::Begin() {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+}
+
+void Gui::End() {
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void Gui::Destroy() {
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui::DestroyContext();
+}
--- Engine/HexCraft.cpp
+++ Engine/HexCraft.cpp
@@ -3,6 +3,7 @@
 #include "Shader.h"
 #include "log.h"
 #include "Camera.h"
+#include "Gui.h"
 
 #include "glm/gtc/matrix_transform.hpp"
 #include <glew.h>
@@ -12,34 +13,54 @@ int main() {
 
 	if (!Context::Init(DEFAULT_RESOLUTION_X, DEFAULT_RESOLUTION_Y))
 		return 0;
+	Context::EnableDepthTest(1);
 
-	Shapes::Init();
-	Shader shader = Shader("Resources\\Shaders\\Color.shader");
-	Camera cam    = Camera(fVec2(DEFAULT_RESOLUTION_X, DEFAULT_RESOLUTION_Y), fVec3(0.0, 0.0, 1.0));
+
+	Shape::Init();
+	Gui::Init();
+
+	Shader shader  = Shader("Resources\\Shaders\\BoxColor.shader");
+	Shader shader2 = Shader("Resources\\Shaders\\BoxColor.shader");
+
+	Camera cam    = Camera(fVec2(DEFAULT_RESOLUTION_X, DEFAULT_RESOLUTION_Y), fVec3(0.0, 0.0, 0.0));
 	cam.Use();
 
-
-	float angle = 0.0f;
+	fVec3 scale	   = fVec3(200.0, 200.0, 1.0);
+	fVec3 position = fVec3(0.0, 0.0, -DEFAULT_ORTHO_PLANE * 2.0);
+	fVec3 rotation = fVec3(0.0, 0.0, 0.0);
+		
 	while (!Context::shouldTerminate) {
-		 bool up   = glfwGetKey((GLFWwindow*)Context::window, 265) == GLFW_PRESS;
-		 bool down = glfwGetKey((GLFWwindow*)Context::window, 264) == GLFW_PRESS;
-		 angle     += (up - down);
-		 
-		 glm::vec3 t = glm::vec3(0.0, 0.0, -2.0);
-		 glm::mat4 model = glm::translate(glm::mat4(1.0), t);
-		 model = glm::rotate(model, Maths::DegToRad(angle), glm::vec3(1.0, 0.0, 0.0));
+		Gui::Begin();
 
+		ImGui::Begin("Debug");
+		ImGui::DragFloat3("Scale", &scale.x);
+		ImGui::DragFloat3("Position", &position.x);
+		ImGui::DragFloat3("Rotation", &rotation.x);
+		ImGui::End();
+		 
+		 if (glGetError()) 
+			 printf("Error");
+
+		 Context::Clear(0.04,0.04,0.1);
+
+		 glm::mat4 model = glm::translate(glm::mat4(1.0), glm::vec3(position.x, position.y, position.z));
+
+		 model = glm::rotate(model, Maths::DegToRad(rotation.x), glm::vec3(1.0,0.0,0.0));
+		 model = glm::rotate(model, Maths::DegToRad(rotation.y), glm::vec3(0.0,1.0,0.0));
+		 model = glm::rotate(model, Maths::DegToRad(rotation.z), glm::vec3(0.0,0.0,1.0));
+
+	     model = glm::scale(model, glm::vec3(scale.x, scale.y, scale.z));
 		 shader.Use();
-		 //shader.SetUniform(GLSLDataType::Float3, "uModel", { &t });
 		 glm::mat4 temp = cam.activeCam->VP * model;
 		 shader.SetUniform(GLSLDataType::Mat4f, "uMVP", { &temp[0][0] });
 
+		 Cube::instance.Draw();
 
-		 if (glGetError()) 
-			 printf("Error");
-		 Context::Clear(0.2,0.2,0.3);
-		 Quad::instance.Draw();
+		 Gui::End();
 		 Context::Update();
 	}
+
+	Shape::Destroy();
+	Gui::Destroy();
 	Context::Destroy();
 }
