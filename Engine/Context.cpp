62143ea6bfe4a0297fc9fb95309e759bf9ca89ab
Wildric <wildric.auric@gmail.com>
2023-05-19 01:37:31 +0200

    added depth test, face culling and MSAA

--- Engine/Context.cpp
+++ Engine/Context.cpp
@@ -14,6 +14,18 @@ void WindowSizeCallBack(GLFWwindow* window, int width, int height) {
 	Context::__height = height;
 }
 
+uint32 Context::__depthTestMask = 0;
+
+void Context::EnableDepthTest(bool status) {
+	if (status) {
+		Context::__depthTestMask = GL_DEPTH_BUFFER_BIT;
+		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
+		return;
+	}
+	Context::__depthTestMask = 0;
+	glDisable(GL_DEPTH_TEST);
+}
 
 bool Context::Init(int width, int height) {
 
@@ -24,6 +36,10 @@ bool Context::Init(int width, int height) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); //Maybe 4 later for compute shaders
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//Temporary multisampling, main framebuffer multisampling, would need to do it offscreen later
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glEnable(GL_MULTISAMPLE);
+
 	Context::window = (void*)glfwCreateWindow(width, height,"HexCraft", NULL, NULL);
 	if (Context::window == nullptr) {
 		const char* buffer = "";
@@ -39,12 +55,17 @@ bool Context::Init(int width, int height) {
 		LOG("Error during glew initialization");
 		return 0;
 	}
+
+	glEnable(GL_CULL_FACE);  //glFrontFace(GL_FRONT) is default setting
+
 	return 1;
 }
 
 void Context::Clear(float r, float g, float b, float a) {
 	glClearColor(r, g, b, a);
-	glClear(GL_COLOR_BUFFER_BIT); //TODO::Add Dynamic clearing for depth buffer and stencil buffer if enabled
+	GLbitfield mask = GL_COLOR_BUFFER_BIT;
+	mask |= Context::__depthTestMask;
+	glClear(mask); //TODO::Add Dynamic clearing for depth buffer and stencil buffer if enabled
 }
 
 void Context::Update() {
--- Engine/Context.h
+++ Engine/Context.h
@@ -1,14 +1,16 @@
 #pragma once
-
+#include "Globals.h"
 
 class Context {
 public:
 	static void* window;
 	static int __width; //ReadOnly
 	static int __height; 
+	static uint32 __depthTestMask;
 	static int shouldTerminate;
 	static int	vSync;
 	static bool Init(int width = 640, int height = 480);
+	static void EnableDepthTest(bool status);
 	static void Clear(float r = 0.0f, float g = 0.0f, float b = 0.0f, float a = 0.0f );
 	static void Update();
 	static void Destroy();
