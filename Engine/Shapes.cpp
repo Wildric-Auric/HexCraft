07877991464ecf5f000ba36cfddc26e1eb5fce2b
Wildric <wildric.auric@gmail.com>
2023-05-19 01:44:12 +0200

    added shape base class and cube class

--- Engine/Shapes.cpp
+++ Engine/Shapes.cpp
@@ -4,27 +4,32 @@
 
 
 
-void Shapes::Init() {
-	Quad::instance = Quad(nullptr);
+void Shape::Init() {
+	Quad::instance	   = Quad(nullptr);
+	Triangle::instance = Triangle(nullptr);
+	Cube::instance     = Cube(nullptr);
 }
 
-void Shapes::Destroy() {
+void Shape::Destroy() {
 	Quad::instance.Delete();
+	Triangle::instance.Delete();
 }
-
-Quad Quad::instance;
-
-
-//uint8 Triangle::indices[3] = { 0, 1, 2 };
-
-uint32 Quad::indices[6]	   = { 0, 1, 2, 1, 3, 2 };
-
 
 Vertex::Vertex(fVec3 position, fVec2 uv) {
 	this->position = position;
 	this->uv       = uv;
 }
 
+CubeVertex::CubeVertex(fVec3 position, fVec2 uv, float textureIndex) {
+	this->position	   = position;
+	this->uv		   = uv;
+	this->textureIndex = textureIndex;
+}
+
+//Quad----------------------------------------
+
+Quad Quad::instance;
+uint32 Quad::indices[6]	   = { 0, 1, 2, 1, 3, 2 };
 Vertex Quad::vertices[4] = {
 	Vertex({ -0.5, -0.5, 0.0}, {0.0, 0.0 }),
 	Vertex({  0.5, -0.5, 0.0}, {1.0, 0.0 }),
@@ -32,7 +37,6 @@ Vertex Quad::vertices[4] = {
 	Vertex({  0.5, 0.5, 0.0} , {1.0, 1.0 })
 };
 
-#include <iostream>
 void Quad::SetUpVerticesData(const Vertex* const vert) {
 	//Generate and bind
 	glGenBuffers(1, &this->EBO);
@@ -43,7 +47,7 @@ void Quad::SetUpVerticesData(const Vertex* const vert) {
 	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
 	//Fill buffers
-	glBufferData(GL_ARRAY_BUFFER, 4*sizeof(Vertex), &(vert[0].position.x), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Quad::vertices), &(vert[0].position.x), GL_STATIC_DRAW);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Quad::indices), Quad::indices, GL_STATIC_DRAW);
 	//Enable attributes
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
@@ -65,14 +69,9 @@ Quad::Quad(const Vertex* const vert) {
 	SetUpVerticesData(vert);
 }
 
-void Quad::Init() {
-	Quad::instance = Quad(nullptr);
-}
-
-
 void Quad::Draw() {
 	glBindVertexArray(this->VAO);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, sizeof(Quad::indices) / sizeof(Quad::indices[0]), GL_UNSIGNED_INT, 0);
 }
 
 void Quad::Delete() {
@@ -82,14 +81,136 @@ void Quad::Delete() {
 	this->EBO = 0; this->VAO = 0; this->VBO = 0;
 }
 
+//Triangle---------------------------
 
+Triangle Triangle::instance;
+uint32 Triangle::indices[3]  = { 0, 1, 2 };
+Vertex Triangle::vertices[3] = {
+	Vertex({ -0.5, -0.5, 0.0 },{ 0.0, 0.0 }),
+	Vertex({ 0.5, -0.5, 0.0 }, { 1.0, 0.0 }),
+	Vertex({ -0.5, 0.5, 0.0 }, { 0.0, 1.0 })
+};
 
+Triangle::Triangle(const Vertex* const vert) {
+	if (vert == nullptr) {
+		SetUpVerticesData(Triangle::vertices);
+		return;
+	}
+	SetUpVerticesData(vert);
+}
 
-//void Triangle::Init() {
-//
-//}
-//
-//void Triangle::Delete() {
-//	
-//}
+void Triangle::SetUpVerticesData(const Vertex* const vert) {
+	
+	glGenBuffers(1, &this->EBO);
+	glGenBuffers(1, &this->VBO);
+	glGenVertexArrays(1, &this->VAO);
+
+	glBindVertexArray(this->VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+	
+	glBufferData(GL_ARRAY_BUFFER		, sizeof(Triangle::vertices), &(vert[0].position.x), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Triangle::indices) , Triangle::indices, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+	glEnableVertexAttribArray(0); 
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)sizeof(Vertex::position));
+	glEnableVertexAttribArray(1); 
+	
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+}
+
+void Triangle::Draw() {
+	glBindVertexArray(this->VAO);
+	glDrawElements(GL_TRIANGLES, sizeof(Triangle::indices) / sizeof(Triangle::indices[0]), GL_UNSIGNED_INT, 0);
+}
+
+void Triangle::Delete() {
+	glDeleteVertexArrays(1, &this->VAO);
+	glDeleteBuffers(1, &this->VBO);
+	glDeleteBuffers(1, &this->EBO);
+	this->EBO = 0; this->VAO = 0; this->VBO = 0;
+}
+
+//Cube------------------------------------------
+
+Cube Cube::instance;
+uint32 Cube::indices[36] = { 
+	0, 1, 2, 1, 3, 2,     //Front face
+	4, 6, 3, 3, 1, 4,     //Right face
+	4, 5, 6, 5, 7, 6,	  //Back face
+	0, 2, 5, 5, 2, 7,     //Left face
+	8, 10,9, 9, 10,3,     //Upper face
+	0,12,11,11,12,13      //Lower face
+};
+CubeVertex Cube::vertices[14] = {
+	//Front face
+	CubeVertex({ -0.5, -0.5 , 0.5 }, { 0.0, 0.0 }, 0),
+	CubeVertex({  0.5, -0.5 , 0.5 }, { 1.0, 0.0 }, 0),
+	CubeVertex({ -0.5,  0.5 , 0.5 }, { 0.0, 1.0 }, 0),
+	CubeVertex({  0.5,  0.5,  0.5 }, { 1.0, 1.0 }, 0),
+	//Back face
+	CubeVertex({   0.5, -0.5 , -0.5 }, { 0.0, 0.0 }, 0),
+	CubeVertex({  -0.5, -0.5 , -0.5 }, { 1.0, 0.0 }, 0),
+	CubeVertex({   0.5,  0.5 , -0.5 }, { 0.0, 1.0 }, 0),
+	CubeVertex({  -0.5,  0.5 , -0.5 }, { 1.0, 1.0 }, 0),
+	//Upper face
+	CubeVertex({  -0.5,  0.5 , -0.5 }, { 0.0, 0.0 }, 0),
+	CubeVertex({   0.5,  0.5 , -0.5 }, { 1.0, 0.0 }, 0),
+	CubeVertex({  -0.5,  0.5 ,  0.5 }, { 0.0, 1.0 }, 0),
+	//Lower face
+	CubeVertex({   0.5,  -0.5 , 0.5 }, { 1.0, 0.0 }, 0),
+	CubeVertex({  -0.5,  -0.5 , -0.5 },{ 0.0, 1.0 }, 0),
+	CubeVertex({   0.5,  -0.5 , -0.5 },{ 1.0, 1.0 }, 0),
+
+};
+
+Cube::Cube(const CubeVertex* const vert) {
+	if (vert == nullptr) {
+		SetUpVerticesData(Cube::vertices);
+		return;
+	}
+	SetUpVerticesData(vert);
+}
+
+void Cube::SetUpVerticesData(const CubeVertex* const vert) {
+
+	glGenBuffers(1, &this->EBO);
+	glGenBuffers(1, &this->VBO);
+	glGenVertexArrays(1, &this->VAO);
+
+	glBindVertexArray(this->VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->EBO);
+
+	glBufferData(GL_ARRAY_BUFFER,		  sizeof(Cube::vertices), &(vert[0].position.x), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Cube::indices) ,  Cube::indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(CubeVertex), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(CubeVertex), (void*)sizeof(CubeVertex::position));
+	glEnableVertexAttribArray(1);
+
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+void Cube::Draw() {
+	glBindVertexArray(this->VAO);
+	glDrawElements(GL_TRIANGLES, sizeof(Cube::indices) / sizeof(Cube::indices[0]), GL_UNSIGNED_INT, 0);
+}
+
+void Cube::Delete() {
+	glDeleteVertexArrays(1, &this->VAO);
+	glDeleteBuffers(1, &this->VBO);
+	glDeleteBuffers(1, &this->EBO);
+	this->EBO = 0; this->VAO = 0; this->VBO = 0;
+}
+
 
--- Engine/Shapes.h
+++ Engine/Shapes.h
@@ -1,43 +1,48 @@
 #include "Globals.h"
 
-
-
-
-
-class Shapes {
-public:
-	static void Init();
-	static void Destroy();
-};
-
-
-
-
-
-
 struct Vertex {
 	Vertex(fVec3 position, fVec2 uv);
 	fVec3 position;
 	fVec2 uv;
 };
 
-//class Triangle {
-//private:
-//	uint32 VBO, VAO, EBO = 0;
-//public:
-//	static Triangle instance;
-//	static uint8 indices[3];
-//
-//	static void Init();
-//	Triangle() {};
-//	Triangle(iVec2 size);
-//
-//	iVec2 size;
-//	Vertex vertices[3];
-//	void Delete();
-//};
+struct CubeVertex {
+	CubeVertex(fVec3 position, fVec2 uv, float textureIndex);
+	fVec3 position;
+	fVec2 uv;
+	float textureIndex;
+};
 
-class Quad {
+class Shape {
+public:
+	virtual void SetUpVerticesData(const Vertex* const vert) {};
+	virtual void Draw()   {};
+	virtual void Delete() {};
+
+	static void Init();
+	static void Destroy();
+};
+
+
+class Triangle : public Shape {
+private:
+	uint32 VBO = 0;
+	uint32 VAO = 0;
+	uint32 EBO = 0;
+public:
+	static Triangle instance;
+	static uint32 indices[3];
+	static Vertex vertices[3];
+
+	Triangle() {};
+	Triangle(const Vertex* const vert);
+
+	void SetUpVerticesData(const Vertex* const vert) override;
+	void Draw()										 override;
+	void Delete()									 override;
+};
+
+class Quad : public Shape {
 private: 
 	uint32 VBO = 0;
 	uint32 VAO = 0;
@@ -45,14 +50,33 @@ private:
 public:
 	static Quad  instance;
 	static uint32 indices[6];
-	static void Init();
 	static Vertex vertices[4];
 
-	void SetUpVerticesData(const Vertex* const vert);
 
 	Quad() {};
 	//Default constructor: A quad of 1 unit size centered on screen if vert = nullptr
 	Quad(const Vertex* const vert);
-	void Draw();
-	void Delete();
+
+	void SetUpVerticesData(const Vertex* const vert) override;
+	void Draw()                                      override;
+	void Delete()									 override;
+};
+
+class Cube : public Shape {
+private:
+	uint32 VBO = 0;
+	uint32 VAO = 0;
+	uint32 EBO = 0;
+public:
+	static Cube instance;
+	static uint32 indices[36];
+	static CubeVertex vertices[14];
+	
+	Cube() {};
+
+	Cube(const CubeVertex* const vert);
+
+	void SetUpVerticesData(const CubeVertex* const vert);
+	void Draw()										     override;
+	void Delete()										 override;
 };
