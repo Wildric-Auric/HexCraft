4ddd7ff87c30600c2a663999cf1c6908f276a869
Wildric <wildric.auric@gmail.com>
2023-05-24 12:32:55 +0200

    added macro for warning printing

--- Engine/log.h
+++ Engine/log.h
@@ -2,9 +2,10 @@
 #include <string>
 #ifdef _DEBUG	
 	#include <iostream>
-	#define LOG(str) std::cout << str << std::endl;
+	#define LOG(str) std::cout << str << std::endl
 #else
 	#define LOG(str)
 #endif
 
-#define LOG_WARNING_FILE_CANNOT_OPEN(path) LOG(std::string("Warning cannot open file: ") + path)
+#define LOG_WARNING_FILE_CANNOT_OPEN(path) LOG(std::string("Warning::Cannot open file: ") + path)
+#define LOG_WARNING(str) LOG(std::string("Warning::") + str)
