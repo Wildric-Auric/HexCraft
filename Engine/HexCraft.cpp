5b20fa2d772138564136dee1c3fff8b514126676
Wildric <wildric.auric@gmail.com>
2023-05-24 12:49:11 +0200

    updated demo scene

--- Engine/HexCraft.cpp
+++ Engine/HexCraft.cpp
@@ -4,9 +4,13 @@
 #include "log.h"
 #include "Camera.h"
 #include "Gui.h"
-#include <vector>
+#include "Texture.h"
 
 #include "glm/gtc/matrix_transform.hpp"
+
+
+#include <vector>
+
 
 
 static std::vector<fVec3> mapTest = {};
@@ -36,16 +40,24 @@ int main() {
 	Shader shader  = Shader("Resources\\Shaders\\BoxColor.glsl");
 	Shader shader2 = Shader("Resources\\Shaders\\Color.glsl");
 
-	Camera cam    = Camera(fVec2(DEFAULT_RESOLUTION_X, DEFAULT_RESOLUTION_Y), fVec3(0.0, 0.0, 0.0));
+	Camera cam    = Camera(fVec2(DEFAULT_RESOLUTION_X, DEFAULT_RESOLUTION_Y), fVec3(0.0, 1250.0, 731.0), fVec2(0.0, -31.0));
 	cam.Use();
 
 	fVec3 scale	   = fVec3(200.0, 200.0, 200.0);
-	fVec3 position = fVec3(0.0, 0.0, -DEFAULT_ORTHO_PLANE * 2.0);
+	fVec3 position = fVec3(0.0, 1250.0,-731.0);
 	fVec3 rotation = fVec3(0.0, 0.0, 0.0);
 
 	fVec3 cameraPosition = fVec3();
 	fVec2 cameraRotation = fVec2();
-		
+
+	Image   im  = Image("Resources\\Images\\GrassHex.png");
+	Texture tex = Texture(im);
+
+	float specularStrength = 0.0f;
+	float specularPower    = 256.0f;
+
+	tex.Bind();
+
 	while (!Context::shouldTerminate) {
 		Gui::Begin();
 
@@ -53,6 +65,8 @@ int main() {
 		ImGui::DragFloat3("Scale", &scale.x);
 		ImGui::DragFloat3("Position", &position.x);
 		ImGui::DragFloat3("Rotation", &rotation.x);
+		ImGui::DragFloat( "Specular strength"        , &specularStrength);
+		ImGui::DragFloat( "Specular Power"           , &specularPower);
 
 		bool a = ImGui::DragFloat3("CamPosition", &cameraPosition.x);
 		bool b = ImGui::DragFloat2("CamRotation", &cameraRotation.x);
@@ -76,12 +90,16 @@ int main() {
 		 model = glm::scale(model, glm::vec3(scale.x, scale.y, scale.z));
 		
 		 glm::mat4 temp = cam.activeCam->VP;
+		
 		 shader.SetUniform(GLSLDataType::Mat4f, "uVP",	  { &temp[0][0] });
 		 shader.SetUniform(GLSLDataType::Mat4f, "uModel", { &model[0][0]});
 
 		 shader.SetUniform(GLSLDataType::Float3, "uCamPosition", {&cam.__position});
 		 shader.SetUniform(GLSLDataType::Float3, "uLightSource", {&position});
 
+		 shader.SetUniform(GLSLDataType::Float, "uSpecularStrength", {&specularStrength});
+		 shader.SetUniform(GLSLDataType::Float, "uSpecularPower", {&specularPower});
+		 
 		 HexaPrism::instance.Draw();
 		 //Drawing the map
 		 for (const fVec3& pos : mapTest) {
