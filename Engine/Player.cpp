67a10c888ea2368b804349b79582112773f52bc0
Wildric <wildric.auric@gmail.com>
2023-05-26 22:20:24 +0200

    implemented player's camera control

--- Engine/Player.cpp
+++ Engine/Player.cpp
@@ -2,25 +2,30 @@
 #include "Camera.h"
 #include "Inputs.h"
 void Player::Start() {
-	Inputs::keyBoardMode = KeyBoardMode::INPUT_AZERTY;
+	//Inputs::keyBoardMode = KeyBoardMode::INPUT_AZERTY;
 }
 
 
-//void Player::Update() {
-//	float xDir = Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_RIGHT) - Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_LEFT);
-//	float yDir = Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_UP) - Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_DOWN);
-//
-//	
-//	float xDir0 = Inputs::KeyPressed(HEX_INPUT_KEY_RIGHT) - Inputs::KeyPressed(HEX_INPUT_KEY_LEFT);
-//	float yDir0 = Inputs::KeyPressed(HEX_INPUT_KEY_UP)    - Inputs::KeyPressed(HEX_INPUT_KEY_DOWN);
-//
-//	fVec3 speed0 = 
-//	this->position   = this->position        + fVec3(xDir, yDir).normalize() * speed0 * (Inputs::KeyPressed(HEX_INPUT_KEY_LEFT_SHIFT) ? acceleration : 1.0);
-//	this->rotation   = this->rotation + fVec2(xDir0, yDir0).normalize() * speed * (Inputs::KeyPressed(HEX_INPUT_KEY_LEFT_SHIFT) ? acceleration : 1.0);
-//	this->rotation.x = Maths::Clamp<float>(this->rotation.x, -89.8, 89.8);
-//	this->rotation.y = Maths::Clamp<float>(this->rotation.y, -89.8, 89.8);
-//	
-//	Camera::activeCam->UpdateTranform(this->position, this->rotation);
-//}
-//
-//
+void Player::Update() {
+	float xDir = Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_RIGHT) - Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_LEFT);
+	float yDir = Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_UP) - Inputs::DirectionalKeyPressed(DirectionalKey::INPUT_DOWN);
+	fVec2 movDir = fVec2(xDir, yDir).normalize();
+	
+	float xDir0   = Inputs::KeyPressed(HEX_INPUT_KEY_LEFT) - Inputs::KeyPressed(HEX_INPUT_KEY_RIGHT);
+	float yDir0   = Inputs::KeyPressed(HEX_INPUT_KEY_UP)    - Inputs::KeyPressed(HEX_INPUT_KEY_DOWN);
+	
+	fVec3 forward = Maths::SphericalToCartesian(Camera::activeCam->__rotation);
+	fVec3 right   = forward.Cross(fVec3(0.0, 1.0, 0.0));
+
+	fVec3 result     =  (right * movDir.x * speed.x + forward * movDir.y * speed.y) * (Inputs::KeyPressed(HEX_INPUT_KEY_LEFT_SHIFT) ? acceleration : 1.0);
+
+    this->position   = this->position +  result;
+
+	this->rotation   = this->rotation  + fVec2(xDir0, yDir0).normalize() * sensitivty * 
+					 (Inputs::KeyPressed(HEX_INPUT_KEY_LEFT_SHIFT) ? acceleration : 1.0);
+	this->rotation.y = Maths::Clamp<float>(this->rotation.y, -89.8, 89.8);
+	
+	Camera::activeCam->UpdateTranform(this->position, this->rotation);
+}
+
+
--- Engine/Player.h
+++ Engine/Player.h
@@ -2,10 +2,11 @@
 #include "Globals.h"
 
 class Player {
+public:
 	fVec3 position;
 	fVec2 rotation;
 
-	fVec2 speed        = fVec2(5000.0f, 5000.0f);
+	fVec2 speed        = fVec2(500.0, 500.0f);
 	fVec2 sensitivty   = fVec2(5.0, 5.0);
 	float acceleration = 3.0;
 	float rotAcc       = 3.0;
