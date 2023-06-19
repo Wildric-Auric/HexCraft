732def5051b05c0914d1571bf0642518d8d09a8a
Wildric <wildric.auric@gmail.com>
2023-06-19 15:21:00 +0200

    adapted UnitHex for resources manager

--- Game/UnitHex.cpp
+++ Game/UnitHex.cpp
@@ -3,6 +3,15 @@
 #include "Camera.h"
 #include "CoordinateSystem.h"
 #include "glm/gtc/matrix_transform.hpp"
+#include "Batch.h"
+#include "Log.h"
+#include "ResourcesManager.h"
+#include "Shader.h"
+#include "Texture.h"
+
+
+std::unordered_map<std::string, UnitHex> UnitHex::blockTypes;
+
 
 UnitHex::UnitHex(Shader* shader, Texture* texture, const fVec3& position) {
 	this->shader   = shader;
@@ -10,26 +19,45 @@ UnitHex::UnitHex(Shader* shader, Texture* texture, const fVec3& position) {
 	this->position = position;
 }
 
+Shader* UnitHex::GetShader() {
+	return this->shader;
+}
+
+Texture* UnitHex::GetTexture() {
+	return this->texture;
+}
 
 void UnitHex::Draw(const fVec3& worldPos) {
-
 	glm::mat4 model = glm::mat4(1.0);
-
 	model = glm::translate(model, glm::vec3(worldPos.x, worldPos.y, worldPos.z));
 	model = glm::scale(model, glm::vec3(this->size.x, this->size.y, this->size.z));
 
-	this->shader->Use();
-	this->texture->Bind();
-
-	this->shader->SetUniform(GLSLDataType::Mat4f, "uVP", { &Camera::activeCam->VP[0][0] });
-	this->shader->SetUniform(GLSLDataType::Mat4f, "uModel", { &model[0][0] });
-
-	HexaPrism::instance.Draw();
+	if (!isBatched) {
+		this->texture->Bind();
+		this->shader->Use();
+		this->shader->SetUniform(GLSLDataType::Mat4f, "uVP", { &Camera::activeCam->VP[0][0] });
+		this->shader->SetUniform(GLSLDataType::Mat4f, "uModel", { &model[0][0] });
+		HexaPrism::instance.Draw();
+		return;
+	}
+	Batch::Add(this, VertexData((float*)HexaPrism::vertices, 24 * 9), IndexData(HexaPrism::indices, 60), (void*)&model);
 }
 
 
 void UnitHex::Draw() {
-	fVec3 worldPos = CoordinateSystem::HexToWorld(this->position,this->size);
+	fVec3 worldPos = CoordinateSystem::HexToWorld(this->position, this->size);
 	this->Draw(worldPos);
 }
 
+
+UnitHex* UnitHex::GetBlockType(std::string blockName) {
+	auto temp = UnitHex::blockTypes.find(blockName);
+	if (temp == UnitHex::blockTypes.end()) 
+		return nullptr;
+	return &temp->second;
+}
+
+void UnitHex::Init() {
+	UnitHex::blockTypes["Grass"] = UnitHex(ResourceManager<Shader>::Load(RES_SHADER_DIR "PrismBatched.glsl"),
+										   ResourceManager<Texture>::Load(RES_IMAGE_DIR "GrassHex.png"));
+}
--- Game/UnitHex.h
+++ Game/UnitHex.h
@@ -1,21 +1,23 @@
 //TODO::Change this abstraction so that units are CPU and GPU batched
 #pragma once
 #include "Globals.h"
-#include "Shader.h"
-#include "Texture.h"
+#include "Object.h"
 
 
 
-class UnitHex {
+class UnitHex : public Object {
 public:
 	UnitHex() {};
 	UnitHex(Shader* shader, Texture* texture, const fVec3& position = fVec3());
 	
 	//Will draw at corresponding member position
-	void Draw();
+	void Draw()       override;
+	Shader*  GetShader()  override;
+	Texture* GetTexture() override;
 
 	//Parameter in world Coordinates
 	void Draw(const fVec3& worldPosition);
+
 
 	Shader*  shader  = nullptr;
 	Texture* texture = nullptr;
@@ -23,4 +25,9 @@ public:
 	fVec3   size   = fVec3(500.0f, 500.0f, 500.0f);
 	//Position is in integer coordinates (0,0), (1,0,0) for example; not world pixel coordinates
 	fVec3 position;
+	bool  isBatched = 1;
+
+	static std::unordered_map<std::string, UnitHex> blockTypes;
+	static UnitHex* GetBlockType(std::string blockName);
+	static void Init();
 };
