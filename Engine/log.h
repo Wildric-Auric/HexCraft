432d77dd1cc1a7b92d3ef02fee49ce0bb5807b65
Wildric <wildric.auric@gmail.com>
2023-06-19 14:39:12 +0200

    added opengl log errors

--- Engine/log.h
+++ Engine/log.h
@@ -2,9 +2,13 @@
 #include <string>
 #ifdef _DEBUG	
 	#include <iostream>
+	#include <glew.h>
 	#define LOG(str) std::cout << str << std::endl
+	#define LOG_OPENGL_ERROR() if (int __log_err = glGetError())\
+										LOG(std::to_string(__log_err));
 #else
 	#define LOG(str)
+	#define LOG_OPENGL_ERROR() 
 #endif
 
 #define LOG_WARNING_FILE_CANNOT_OPEN(path) LOG(std::string("Warning::Cannot open file: ") + path)
