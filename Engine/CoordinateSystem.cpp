bede5044d74078ace072c8bb5d34178a202f9b85
Wildric <wildric.auric@gmail.com>
2023-06-19 14:38:42 +0200

    added chunk coordinates conversion

--- Engine/CoordinateSystem.cpp
+++ Engine/CoordinateSystem.cpp
@@ -8,12 +8,57 @@ fVec3 CoordinateSystem::HexToWorld(const fVec3& coord, const fVec3& unitSize) {
 	return ret;
 }
 
-fVec3 CoordinateSystem::WorldToHex(const fVec3& coord, const fVec3& unitSize) {
-	fVec3 ret;
-	ret.x = (4.0f / 3.0f) * (1.0 / unitSize.x) * coord.x;
-	ret.y = (1.0f / unitSize.y)				   * coord.y;
-	ret.z = (1.0f / unitSize.z)				   * coord.z - (int(ret.x) % 2 != 0 ? 0.5 : 0.0);
-	return Maths::Floor(ret);
+fVec3 CoordinateSystem::WorldToHex(const fVec3& c, const fVec3& unitSize) {
+	fVec2 halfSize = fVec2(unitSize.x, unitSize.y) * 0.5;
+	float yCoord         = c.y / unitSize.y;
+	//Center on 0,0
+	fVec2 coord = fVec2(c.x + halfSize.x, c.y + halfSize.y);
+	fVec2 absoluteSign  = fVec2(Maths::Sign(coord.x), Maths::Sign(coord.y));
+	//Operations to get location given coord.x
+	float fractPart = fmod(coord.x, 3.0 * halfSize.x);
+	float intPart   = (float)((int)(coord.x / (3.0 * halfSize.x)));
+
+	//The smaller rectangle
+	if (fractPart >= (2.0 * halfSize.x)) {
+		return fVec3(2.0 * intPart + 1.0, yCoord, coord.y / halfSize.y);
+	}
+	//The widder rectangle
+	fVec3 ret      = fVec3(2.0 * intPart, yCoord, (int)(coord.y * 0.5 / halfSize.y));
+	fVec2 rectPos  = fVec2(3.0 * intPart * halfSize.x + absoluteSign.x * halfSize.x, 
+						   ret.y * 2.0 * halfSize.y + absoluteSign.y * halfSize.y);
+	fVec2 localPos = coord - rectPos;
+	//Restriction to upper right quarter of the rectangle
+	fVec2 localPosRest  = fVec2(abs(localPos.x), abs(localPos.y));
+	fVec2 localSign     = fVec2(Maths::Sign(localPos.x), Maths::Sign(localPos.y));
+	
+	fVec3 recSideVec    = fVec3(fVec2(0.5 * halfSize.x, halfSize.y), 0.0  , 0.0);
+
+
+
+}
+
+
+fVec2 CoordinateSystem::HexToChunck(const fVec3& hexCoord, const fVec2& chunkSize) {
+	int x = 0;
+	int y = 0;
+	if (abs(hexCoord.x) > 0.01)
+		x = hexCoord.x / chunkSize.x;
+	if (abs(hexCoord.z) > 0.01)
+		y = hexCoord.z / chunkSize.y;
+	if (hexCoord.x < 0.0) 
+		x -= 1;
+	if (hexCoord.z < 0.0) 
+		y -= 1;
+	return fVec2(x,y);
+}
+
+fVec2 CoordinateSystem::WorldToChunck(const fVec3& worldCoord, const fVec3& hexUnitSize, const fVec2& chunkSize) {
+	return CoordinateSystem::HexToChunck(CoordinateSystem::WorldToHex(worldCoord, hexUnitSize), chunkSize);
+}
+
+//Result is upper left block coordinate
+fVec3 CoordinateSystem::ChunkToHex(const fVec2& chunkCoord, const fVec2& chunkSize) {
+	return fVec3(chunkSize.x * chunkCoord.x, 0.0, chunkSize.y * chunkCoord.y);
 }
 
 fVec3 CoordinateSystem::WorldToRect(const fVec3& coord, const fVec3& unitSize) {
@@ -31,4 +76,6 @@ fVec3 CoordinateSystem::RectToWorld(const fVec3& coord, const fVec3& unitSize) {
 	ret.z = coord.z * unitSize.z;
 	return ret;
 }
+
+
 
--- Engine/CoordinateSystem.h
+++ Engine/CoordinateSystem.h
@@ -7,6 +7,10 @@ public:
 	static fVec3 WorldToHex(const fVec3& coord, const fVec3& unitSize);
 	static fVec3 HexToWorld(const fVec3& coord, const fVec3& unitSize);
 
+	static fVec2 WorldToChunck(const fVec3& worldCoord, const fVec3& hexUnitSize, const fVec2& chunkSize);
+	static fVec2 HexToChunck(const fVec3& worldCoord, const fVec2& chunkSize);
+	static fVec3 ChunkToHex(const fVec2& chunkCoord, const fVec2& chunkSize);
+
 	static fVec3 WorldToRect(const fVec3& coord, const fVec3& unitSize);
 	static fVec3 RectToWorld(const fVec3& coord, const fVec3& unitSize);
 };
