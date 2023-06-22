e4ab558c1d7dc8a08af288b89776685e86745c0c
Wildric <wildric.auric@gmail.com>
2023-06-22 02:56:58 +0200

    fixed worldtohex conversion

--- Engine/CoordinateSystem.cpp
+++ Engine/CoordinateSystem.cpp
@@ -9,32 +9,42 @@ fVec3 CoordinateSystem::HexToWorld(const fVec3& coord, const fVec3& unitSize) {
 }
 
 fVec3 CoordinateSystem::WorldToHex(const fVec3& c, const fVec3& unitSize) {
-	fVec2 halfSize = fVec2(unitSize.x, unitSize.y) * 0.5;
+	fVec2 halfSize = fVec2(unitSize.x, unitSize.z) * 0.5;
 	float yCoord         = c.y / unitSize.y;
 	//Center on 0,0
-	fVec2 coord = fVec2(c.x + halfSize.x, c.y + halfSize.y);
+	fVec2 coord = fVec2(c.x + halfSize.x, c.z + halfSize.y);
 	fVec2 absoluteSign  = fVec2(Maths::Sign(coord.x), Maths::Sign(coord.y));
 	//Operations to get location given coord.x
 	float fractPart = fmod(coord.x, 3.0 * halfSize.x);
 	float intPart   = (float)((int)(coord.x / (3.0 * halfSize.x)));
-
+	if (coord.x < 0.0)
+		intPart = intPart - 1.0;
 	//The smaller rectangle
-	if (fractPart >= (2.0 * halfSize.x)) {
-		return fVec3(2.0 * intPart + 1.0, yCoord, coord.y / halfSize.y);
+	if (fractPart >= (2.0 * halfSize.x) || (fractPart <= 0.0 && fractPart > -halfSize.x)) {
+		float  oddOffset = coord.y - halfSize.y;
+		return fVec3( (2.0 * intPart + 1.0), yCoord, (int)(oddOffset * 0.5 / halfSize.y) - (  oddOffset < 0.0 ? 1.0f : 0.0f)); //Fuck this line
 	}
 	//The widder rectangle
-	fVec3 ret      = fVec3(2.0 * intPart, yCoord, (int)(coord.y * 0.5 / halfSize.y));
-	fVec2 rectPos  = fVec2(3.0 * intPart * halfSize.x + absoluteSign.x * halfSize.x, 
-						   ret.y * 2.0 * halfSize.y + absoluteSign.y * halfSize.y);
+	fVec3 ret = fVec3(2.0 * intPart + (absoluteSign.x < 0.0f ? 1.0f : 0.0f), yCoord, (int)(coord.y * 0.5 / halfSize.y));
+	if (absoluteSign.y < 0.0)
+		ret.z      = ret.z - 1.0;
+
+	fVec2 rectPos  = fVec2(3.0   * intPart * halfSize.x +  (1.0 +  2.0 * (float)(absoluteSign.x == -1.0f) ) * halfSize.x, 
+						   2.0   * ret.z   * halfSize.y +  halfSize.y); //DONT MULTIPLY SECOND PART BY absoluteSign.y
 	fVec2 localPos = coord - rectPos;
 	//Restriction to upper right quarter of the rectangle
 	fVec2 localPosRest  = fVec2(abs(localPos.x), abs(localPos.y));
 	fVec2 localSign     = fVec2(Maths::Sign(localPos.x), Maths::Sign(localPos.y));
 	
-	fVec3 recSideVec    = fVec3(fVec2(0.5 * halfSize.x, halfSize.y), 0.0  , 0.0);
+	fVec3 rectSideVec    = fVec3(0.5 * halfSize.x - halfSize.x, halfSize.y, 0.0);
+	fVec3 vec            = fVec3(localPosRest.x - halfSize.x, localPosRest.y, 0.0);
 
-
-
+	fVec3 tempProduct    = vec.Cross(rectSideVec); // the Z component indicates 
+	if (tempProduct.z <= 0.0)
+		return ret;
+	fVec2 temp = fVec2(ret.x, ret.z);
+	temp = temp + fVec2(Maths::Sign(localPos.x), Maths::Min<float>(Maths::Sign(localPos.y), 0.0f));
+	return fVec3(temp.x, ret.y, temp.y);
 }
 
 
@@ -52,7 +62,7 @@ fVec2 CoordinateSystem::HexToChunck(const fVec3& hexCoord, const fVec2& chunkSiz
 	return fVec2(x,y);
 }
 
-fVec2 CoordinateSystem::WorldToChunck(const fVec3& worldCoord, const fVec3& hexUnitSize, const fVec2& chunkSize) {
+fVec2 CoordinateSystem::WorldToChunck(const fVec3& worldCoord, const fVec3& hexUnitSize, const fVec2& chunkSize) { 
 	return CoordinateSystem::HexToChunck(CoordinateSystem::WorldToHex(worldCoord, hexUnitSize), chunkSize);
 }
 
@@ -76,6 +86,3 @@ fVec3 CoordinateSystem::RectToWorld(const fVec3& coord, const fVec3& unitSize) {
 	ret.z = coord.z * unitSize.z;
 	return ret;
 }
-
-
-
